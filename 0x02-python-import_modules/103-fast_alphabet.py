7f34c25472ea87b7808173e04731f0e5a54b55d6
mohamed-elghafiani <medghafiani@gmail.com>
2023-06-14 17:00:37 -0700

    added a program that prints the alphabet in uppercase, followed by a new line

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from print_alphabets import alph_uppercase
+alph_uppercase()
---
+++ 0x02-python-import_modules/print_alphabets.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def alph_uppercase():
+    for i in range(65, 91):
+        print("{}".format(chr(i)), end="")
+    print("")
