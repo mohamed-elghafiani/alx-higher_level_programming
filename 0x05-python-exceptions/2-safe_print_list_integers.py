d751e9e468cb1e154a16b6ae10ebb43365d22eb7
mohamed-elghafiani <medghafiani@gmail.com>
2023-06-26 16:44:32 -0700

    Implemented safe_print_list_integers - handling correctly the return value

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,11 +4,11 @@ def safe_print_list_integers(my_list=[], x=0):
     n = 0
     for i in range(x):
         try:
-            n += 1
             print("{:d}".format(my_list[i]), end="")
+            n += 1
 
         except (ValueError, TypeError):
             pass
 
-    print("")
+    print()
     return n
