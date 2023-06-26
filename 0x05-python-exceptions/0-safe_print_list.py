dcf26b4a55b99b669f17fcf1474cc00ffd84a213
mohamed-elghafiani <medghafiani@gmail.com>
2023-06-26 11:18:33 -0700

    safe_print_list - pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,4 @@
-#1/usr/bin/python3
+#!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
     n = 0
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             n += 1
-    except:
+    except IndexError:
         pass
 
     finally:
