9513efd2d311aadc2ed69cc5e9336c601856f85c
mohamed-elghafiani <medghafiani@gmail.com>
2023-06-27 06:03:19 -0700

    Implemented Square class with a private size attribute for defining a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+        This class represents a square
+
+        Attributes:
+            None
+
+        Methods:
+            None
+    """
+    def __init__(self, size):
+        """
+            Initializes a new instance of the Square class
+        """
+        self.__size = size
