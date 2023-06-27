b456c8092b316304fea136fae9ac5a733cd3e65e
mohamed-elghafiani <medghafiani@gmail.com>
2023-06-27 07:51:31 -0700

    added to the square class based on 4-square my_print that prints in stdout the square with the character #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+
+"""
+    This Module defines the Square class
+"""
+
+
+class Square:
+    """
+        This class represents a square
+
+    """
+
+    def __init__(self, size=0):
+        """
+            Initializes an instance of the Square class
+
+            Args:
+                size (int, optional): the size of the square. Default to 0.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+            Gets the size of the square
+
+            Returns:
+                size (int) : the size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """
+            Sets the size of the square
+
+            Args:
+                size (int): the size of the square
+
+            Raises:
+                TypeError: If size is not an integer
+                ValueError: If size is < 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+            Calculates the area of the square
+
+            Returns:
+                int : The area of the square
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+            Prints in stdout the square with the character #
+        """
+        if self.__size == 0:
+            print()
+
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
