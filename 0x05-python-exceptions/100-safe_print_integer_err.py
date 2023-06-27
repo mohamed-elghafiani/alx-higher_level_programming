453c89029ae0e91a37d5144c1857b4fc9fb6af2d
mohamed-elghafiani <medghafiani@gmail.com>
2023-06-26 17:46:35 -0700

    Implement safe_print_integer_err function for printing an integer with error handling

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    is_int = True
+    try:
+        print("{:d}".format(value))
+
+    except (ValueError, TypeError) as err:
+        is_int = False
+        sys.stderr.write(str(err) + "\n")
+    return is_int
