46e7ea14203f15bdcd70a21206ea6721f56fafd2
mohamed-elghafiani <medghafiani@gmail.com>
2023-06-27 08:41:39 -0700

    handaled negative position parameters

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -65,7 +65,7 @@ class Square:
     def position(self, position):
         """
             Sets teh position of the square
-            
+
             Args:
                 position (tuple): The position of the square
 
@@ -93,13 +93,14 @@ class Square:
         if self.__size == 0:
             print()
             return
-        
+
         if self.__position[1] > 0:
             for i in range(self.__position[1]):
                 print()
 
         for i in range(self.__size):
-            print(" "*self.__position[0], end="")
+            if self.__position[0] > 0:
+                print(" "*self.__position[0], end="")
             for j in range(self.__size):
                 print("#", end="")
-            print() 
+            print()
