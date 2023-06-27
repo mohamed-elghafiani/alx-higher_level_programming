15bf943f2cae4e46f1e74dcb756f62e94721bdde
mohamed-elghafiani <medghafiani@gmail.com>
2023-06-27 07:23:21 -0700

    Implemented Square class with private size attribute, size validation in the constructor, and area method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+    This Module defines the Square class
+"""
+
+
+class Square:
+    """
+       This class represents a square
+    """
+
+    def __init__(self, size=0):
+        """
+            Initializes an instance of the Square class
+
+            args:
+                size(int, optional): the size of the square. Default to 0.
+
+            raises:
+                TypeError: If size is not an integer
+                ValueError: If size is < 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        """
+            This method returns the current square area
+        """
+        return self.__size ** 2
