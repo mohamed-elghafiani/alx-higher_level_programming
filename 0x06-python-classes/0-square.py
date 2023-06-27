396b6dec3b674693d1da16a2f91d12f711770d03
mohamed-elghafiani <medghafiani@gmail.com>
2023-06-27 05:44:53 -0700

    added an empty class Square that defines a square - added docstring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 
 class Square:
+    """
+        This class represents a square
+
+        Attributes:
+            None
+
+        Methods:
+            None
+    """
     pass
