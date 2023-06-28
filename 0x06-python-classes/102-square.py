cc8f248fb40fa979efecfa4d963c31a571e20498
mohamed-elghafiani <medghafiani@gmail.com>
2023-06-28 16:11:21 -0700

    Added comparators based on the square area to the Square class defined in 4-square.py

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+
+"""
+    This Module defines the Square class
+"""
+
+
+class Square:
+    """
+        This class represents a square
+
+    """
+
+    def __init__(self, size=0):
+        """
+            Initializes an instance of the Square class
+
+            Args:
+                size (int, optional): the size of the square. Default to 0.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+            Gets the size of the square
+
+            Returns:
+                size (int) : the size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """
+            Sets the size of the square
+
+            Args:
+                size (int): the size of the square
+
+            Raises:
+                TypeError: If size is not an integer
+                ValueError: If size is < 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+            Calculates the area of the square
+
+            Returns:
+                int : The area of the square
+        """
+        return self.__size ** 2
+
+    def __eq__(self, square):
+        if self.area() == square.area():
+            return True
+        else:
+            return False
+
+    def __lt__(self, square):
+        if self.area() < square.area():
+            return True
+        else:
+            return False
+
+    def __le__(self, square):
+        if self.area() <= square.area():
+            return True
+        else:
+            return False
