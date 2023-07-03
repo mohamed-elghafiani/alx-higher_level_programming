44dd7bdb8b926f116675c41169820c70efae31ee
mohamed-elghafiani <medghafiani@gmail.com>
2023-07-03 04:42:13 -0700

    Implimented text_indentation that prints a text with 2 new lines after each of these characters: ., ? and :

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""This module contains the implimentation of the function `text_indentation`
+
+Basic usage:
+  text = "How are you doing? I'm fine thanks."
+  text_indentation(text)
+"""
+
+
+def text_indentation(text):
+    """prints a text with 2 new lines after each of these characters:
+        ., ? and :
+
+    Args:
+      text: A string
+
+    Raises:
+      TypeError: If text is of type other than string
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    text = text.replace("? ", "?\n\n")
+    text = text.replace(". ", ".\n\n")
+    text = text.replace(": ", ":\n\n")
+
+    # if character is at the end of text
+    for char in "?.:":
+        if char == text[-1]:
+            text = text.replace(char, char + "\n\n")
+
+    print(text)
