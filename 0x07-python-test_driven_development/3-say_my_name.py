15a2078c1ca9b3c7de042de4c9ca4a8465dc1cef
mohamed-elghafiani <medghafiani@gmail.com>
2023-07-03 04:36:18 -0700

    Implimented a simple function that print the name of a person while adding somme doctests

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" This Module contains the definition of the function say_my_name """
+
+
+def say_my_name(first_name, last_name=""):
+    """ say_my_name prints "My name is <first_name> <last_name>
+
+    Args:
+      fist_name: A string representing the firt name
+      last_name: A string representing the last name
+
+    Raises:
+      TypeError: If either first_name or last_name is not a string
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("firt_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
