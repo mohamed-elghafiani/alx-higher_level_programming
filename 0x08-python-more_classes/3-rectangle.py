315ec4102d69cae3e193c0374188da48affe8465
mohamed-elghafiani <medghafiani@gmail.com>
2023-07-03 16:32:55 -0700

    Impliment __str__ to print the rectangle with the character #

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -50,6 +50,9 @@ class Rectangle():
         return (self.__width + self.__height) * 2
 
     def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
         rect_str = ""
         for i in range(self.__height):
             rect_str += "#" * self.__width + "\n"
