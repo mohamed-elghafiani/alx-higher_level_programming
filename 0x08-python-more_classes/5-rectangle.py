a7e17bfe7952bc9793c202972b9ae41b2bda9685
mohamed-elghafiani <medghafiani@gmail.com>
2023-07-03 17:12:56 -0700

    Added a Public class attribute number_of_instances

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -62,4 +62,4 @@ class Rectangle():
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     def __del__(self):
-        print("Bye rectangle ...")
+        print("Bye rectangle...")
