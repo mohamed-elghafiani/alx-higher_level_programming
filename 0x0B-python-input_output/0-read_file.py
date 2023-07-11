c0b16ea1922803ba39a5ea19e168689f31000c0e
mohamed-elghafiani <medghafiani@gmail.com>
2023-07-11 15:37:58 -0700

    added docstrings

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
+"""Reading and printing the contents of a file
+"""
+
+
 def read_file(filename=""):
+    """reads and prints the contents of a file
+    """
     with open(filename, encoding="utf-8") as f:
         content = f.read()
         print(content[:-1])
