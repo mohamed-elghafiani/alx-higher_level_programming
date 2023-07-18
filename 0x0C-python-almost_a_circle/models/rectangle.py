e4ffdbe76b91c456ce7bdc56fbbad4ded81f358f
mohamed-elghafiani <medghafiani@gmail.com>
2023-07-18 15:05:43 -0700

    Updated instance attributes using update(self, *args)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,10 +15,14 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """Returns the width of the rectangle
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Sets the value of rectangle's width
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -27,10 +31,14 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """Returns the rectangle's hight
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Sets the rectangle's height
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -39,10 +47,14 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """Returns the x position of the rectangle
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """Sets the x position of the rectangle
+        """
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
@@ -51,10 +63,14 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """Returns the y position of the rectangle
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """Sets the y position of the rectangle
+        """
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
@@ -62,4 +78,53 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """Returns the rectangle's area
+        """
         return self.__width * self.__height
+
+    def display(self):
+        """Display the rectangle using "#"
+        """
+        for _ in range(self.__y):
+            print()
+
+        for i in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
+
+    def __str__(self):
+        s1 = "[Rectangle] ({})".format(self.id)
+        s2 = "{}/{}".format(self.__x, self.__y)
+        s3 = " - {}/{}".format(self.__width, self.__height)
+        return s1 + s2 + s3
+
+    def update(self, *args):
+        """Update the rectangle instance's public attributes
+        """
+        id, width, height, x, y = (None, ) * 5
+        if args is not None:
+            if len(args) == 1:
+                id = args
+                super().__init__(id)
+            elif len(args) == 2:
+                id, width = args
+                super().__init__(id)
+                self.__width = width
+
+            elif len(args) == 3:
+                id, width, height = args
+                super().__init__(id)
+                self.__width = width
+                self.__height = height
+
+            elif len(args) == 4:
+                id, width, height, x = args
+                super().__init__(id)
+                self.__width = width
+                self.__height = height
+                self.__x = x
+
+            elif len(args) == 5:
+                id, width, height, x, y = args
+                super().__init__(id)
+                self.__width = width
+                self.__height = height
