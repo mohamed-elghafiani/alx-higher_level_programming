2319ae2a7579e8e2aaf2efb5711cc0ba2d365b17
mohamed-elghafiani <medghafiani@gmail.com>
2023-09-15 14:56:12 +0000

    fixed n=0 case

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -4,8 +4,9 @@
 
 def pascal_triangle(n):
     """Returns a list of lists of integers representing
-       the Pascal's triangle of n"""
-    if n < 0:
+       the Pascal's triangle of n
+    """
+    if n <= 0:
         return []
 
     if n == 1:
