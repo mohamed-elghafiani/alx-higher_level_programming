a9451df95ea68265cb5d98d9807bf1bd1c1371c5
mohamed-elghafiani <medghafiani@gmail.com>
2023-09-15 15:50:45 +0000

    Search and update file

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,6 +3,7 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """Search and update file"""
     with open(filename, "r+") as file:
         lines = file.readlines()
         updated_lines = []
