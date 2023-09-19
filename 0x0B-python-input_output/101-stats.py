92ca956608ea4dfae6d108a4daa752099bff82a0
mohamed-elghafiani <medghafiani@gmail.com>
2023-09-19 11:16:27 +0000

    added signal_handler for KeyboardInterrupt

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
+"""Log Parsing Module"""
 import sys
 import signal
 
-# Initialize variables to store metrics
+
 total_file_size = 0
 status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-# Function to handle KeyboardInterrupt (CTRL+C)
 def signal_handler(signal, frame):
+    """handle KeyboardInterrupt"""
     print_stats()
     sys.exit(0)
 
-# Function to print statistics
 def print_stats():
+    """print_stats"""
     print("File size: {}".format(total_file_size))
     for status_code in sorted(status_code_counts):
         if status_code_counts[status_code] > 0:
             print("{}: {}".format(status_code, status_code_counts[status_code]))
 
-# Register the signal handler for KeyboardInterrupt
 signal.signal(signal.SIGINT, signal_handler)
 
 line_count = 0
 
-# Read input line by line
 for line in sys.stdin:
     line_count += 1
     parts = line.split()
@@ -34,7 +33,6 @@ for line in sys.stdin:
         if status_code in status_code_counts:
             status_code_counts[status_code] += 1
 
-    # Check if 10 lines have been processed
     if line_count % 10 == 0:
         print_stats()
 
