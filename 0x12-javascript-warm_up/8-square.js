ede3ac21e704c6029aa7540b3ab859c3e7e62e1b
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-11-13 13:53:14 -0800

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const size = Number(args[2]);
+if (!size) {
+  console.log('Missing size');
+}
+
+for (let i = 0; i < size; i++) {
+  console.log('#'.repeat(size));
+}
