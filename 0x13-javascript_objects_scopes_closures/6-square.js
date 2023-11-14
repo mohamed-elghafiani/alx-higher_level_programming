bd6728c1ac79491061d1cce6beef7a5ed2208b87
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-11-14 15:40:37 -0800

    added charPrint

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Square_ = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends Square_ {
   charPrint (c) {
     if (c) {
       for (let i = 0; i < this.width; i++) {
