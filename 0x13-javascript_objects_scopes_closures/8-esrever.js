9d755d6b818a97caa91f7fb7dc9df574a31e3ae3
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-11-14 15:56:41 -0800

    reverse array

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.reverse();
+  const list_ = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    list_.push(list[i]);
+  }
+  return list_;
 };
