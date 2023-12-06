7eb135e155b2363be96e4278356317b61199bb55
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-12-06 14:27:08 -0800

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,6 +2,7 @@
 # Technical interview preparation:
 """ Finding a peak in a list of unsorted integers. """
 
+
 def find_peak(list_of_integers):
     """Return the peak in a list_of_integers"""
     if not list_of_integers:
@@ -16,10 +17,6 @@ def find_peak(list_of_integers):
 
     if list_of_integers[0] > list_of_integers[1]:
         return list_of_integers[0]
-    else:
-        return list_of_integers[1]
 
     if list_of_integers[-1] > list_of_integers[-2]:
         return list_of_integers[-1]
-    else:
-        return list_of_integers[-2]
