675a08524f8eb2285152a3e0dbdbcbd7e746a996
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-12-07 06:25:32 -0800

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Search API"""
+import requests
+import sys
+
+q = "" if len(sys.argv) < 2 else sys.argv[1]
+url = "http://0.0.0.0:5000/search_user"
+
+with requests.post(url, params={"q": q}) as res:
+    if not res.headers.get('content-Type') == 'application/json':
+        print("Not a validd JSON")
+    elif res.json() == {}:
+        print("No result")
+    else:
+        print(res.json())
+        print(f"[{res.json().get('id')}] {res.json().get('name')}")
