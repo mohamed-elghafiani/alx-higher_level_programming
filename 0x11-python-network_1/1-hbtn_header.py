32a3f93dd4a620903c7da20207f5d923b81d6906
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-12-06 16:36:29 -0800

    Response header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # Response header value
-"""A Python script that takes in a URL, sends a request to the URL and displays
+""" 
+A Python script that takes in a URL, sends a request to the URL and displays
 the value of the X-Request-Id variable found in the header of the response.
 """
 import urllib.request
