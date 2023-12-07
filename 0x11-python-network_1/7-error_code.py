23daa25441f9193d109590b75e685b9c355e2b6e
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-12-06 17:44:57 -0800

    handling HTTPError using requests

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Handling urllib.error.HTTPError"""
+import requests
+import requests.exceptions
+import sys
+
+
+if __name__ == "__main__"
+    try:
+        with requests.get(req) as res:
+            print(res.text)
+    except requests.exceptions.HTTPError as e:
+        print(f"Error code: {e.code}")
