181c1cf3f1d33991f97c957769c60292229f0c90
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-12-06 17:53:09 -0800

    handling HTTPError using requests

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,10 +4,9 @@ import requests
 import requests.exceptions
 import sys
 
-
 if __name__ == "__main__":
-    try:
-        with requests.get(sys.argv[1]) as res:
-            print(res.text)
-    except requests.exceptions.HTTPError as e:
-        print(f"Error code: {e.code}")
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print("{}".format(r.text))
