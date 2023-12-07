8595569ee82c219e12f78b5851e975cb9fbc6262
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-12-07 06:30:11 -0800

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,14 +9,15 @@ import requests
 import sys
 
 
-q = "" if len(sys.argv) < 2 else sys.argv[1]
-url = "http://0.0.0.0:5000/search_user"
+if __name__ == "__main__":
+    q = "" if len(sys.argv) < 2 else sys.argv[1]
+    url = "http://0.0.0.0:5000/search_user"
 
-with requests.post(url, params={"q": q}) as res:
-    if not res.headers.get('content-Type') == 'application/json':
-        print("Not a validd JSON")
-    elif res.json() == {}:
-        print("No result")
-    else:
-        print(res.json())
-        print(f"[{res.json().get('id')}] {res.json().get('name')}")
+    with requests.post(url, params={"q": q}) as res:
+        if not res.headers.get('content-Type') == 'application/json':
+            print("Not a validd JSON")
+        elif res.json() == {}:
+            print("No result")
+        else:
+            print(res.json())
+            print(f"[{res.json().get('id')}] {res.json().get('name')}")
