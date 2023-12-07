30fc72745151c76be632673a22bc9285b01b9e8d
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-12-06 16:11:38 -0800

    fetch for status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 
 with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
     data = res.read()
-    print(f"""Body response:
-    - type: {type(data)}
-    - content: {data}
-    - utf8 content: {data.decode("utf-8")}""")
+    print("Body response:")
+    print(f"\t- type: {type(data)}")
+    print(f"\t- content: {data}")
+    print(f"\t- utf8 content: {data.decode('utf-8')}")
