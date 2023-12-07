65efdbb350cae31c884041ed4d726482677e8387
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-12-06 16:43:14 -0800

    extract http headers

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Response header value
-""" 
+"""
 A Python script that takes in a URL, sends a request to the URL and displays
 the value of the X-Request-Id variable found in the header of the response.
 """
@@ -11,5 +11,5 @@ import sys
 if __name__ == "__main__":
     argv = sys.argv
     with urllib.request.urlopen(argv[1]) as req:
-        x_request_id = dict(req.headers._headers)["X-Request-Id"]
+        x_request_id = dict(req.headers._headers).get("X-Request-Id")
         print(x_request_id)
