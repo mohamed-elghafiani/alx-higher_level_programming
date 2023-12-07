25ae457e3a87c162cf51dd2d4207fa97eb54f10f
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-12-06 17:01:56 -0800

    post an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,11 +7,11 @@ import sys
 if __name__ == "__main__":
     argv = sys.argv
     url = argv[1]
-    values = {'email' : argv[2]}
+    values = {'email': argv[2]}
 
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
-       body = response.read()
-       print(body.decode('utf-8'))
+        body = response.read()
+        print(body.decode('utf-8'))
